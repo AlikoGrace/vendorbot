196dc299e776671631010c117b9fe69f0b480239
aliko <gracealiko09@gmail.com>
2025-05-10 15:29:36 +0000

    text user after sign up

--- app/actions/signup.ts
+++ app/actions/signup.ts
@@ -1,6 +1,12 @@
 "use server";
 
 import Prisma from "@/lib/prisma";
+import { Twilio } from "twilio";
+
+const twilioClient = new Twilio(
+  process.env.TWILIO_ACCOUNT_SID as string,
+  process.env.TWILIO_AUTH_TOKEN as string
+);
 
 export const signup = async (formData: FormData) => {
   console.log("formData", formData);
@@ -13,5 +19,11 @@ export const signup = async (formData: FormData) => {
       phone_number: formData.get("whatsappNumber") as string,
       bot_name: `${businessName}'s Bot`,
     },
+  });
+
+  await twilioClient.messages.create({
+    body: `Hi ${businessName}, I am venderbot. What would you like to name me?`,
+    to: "whatsapp:+233507989633",
+    from: "whatsapp:+14155238886",
   });
 };
