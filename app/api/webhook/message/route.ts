03a19de85e1d79518396d98505a2b8b2fd123140
Owbird <owbirdphorccuo@gmail.com>
2025-05-19 01:25:32 +0000

    feat (webhook): Handle botname update command

--- app/api/webhook/message/route.ts
+++ app/api/webhook/message/route.ts
@@ -1,6 +1,37 @@
 import Prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
-import MessagingResponse from "twilio/lib/twiml/MessagingResponse";
+
+type WebhookResponse = {
+  object: "whatsapp_business_account";
+  entry: {
+    id: string;
+    changes: {
+      field: "messages";
+      value: {
+        messaging_product: "whatsapp";
+        metadata: {
+          display_phone_number: string;
+          phone_number_id: string;
+        };
+        contacts: {
+          profile: {
+            name: string;
+          };
+          wa_id: string;
+        }[];
+        messages: {
+          from: string;
+          id: string;
+          timestamp: string;
+          type: "text";
+          text: {
+            body: string;
+          };
+        }[];
+      };
+    }[];
+  }[];
+};
 
 export async function GET(req: NextRequest) {
   const challenge = new URL(req.url).searchParams.get("hub_challenge");
@@ -9,32 +40,57 @@ export async function GET(req: NextRequest) {
 }
 
 export async function POST(req: NextRequest) {
-  const body = await req.text();
-  const params = new URLSearchParams(body);
-  const messageBody = params.get("Body")?.toLowerCase();
-  const vendor = params.get("From")?.replaceAll("whatsapp:", "");
+  const { entry } = (await req.json()) as WebhookResponse;
 
-  const twiml = new MessagingResponse();
+  const {
+    from,
+    text: { body: messageBody },
+  } = entry[0].changes[0].value.messages[0];
 
-  if (messageBody?.includes("/botname")) {
+  if (messageBody.includes("/botname")) {
     const botName = messageBody.split("/botname")[1].toString().trim();
 
     await Prisma.vendors.update({
       where: {
-        phone_number: "0558344624", //vendor,
+        phone_number: "0558344624",
       },
       data: {
         bot_name: botName,
       },
     });
 
-    twiml.message(`Your assistant is called, ${botName}!`);
+    const to = "+233265542141";
+
+    const url = `https://graph.facebook.com/v22.0/562679923606696/messages`;
+
+    const token = process.env.FB_TOKEN;
+
+    const payload = {
+      to,
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      type: "text",
+      text: {
+        body: `Great! Your assistant is now called ${botName}`,
+      },
+    };
+
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+      signal: AbortSignal.timeout(5000)
+    });
+
+
+    console.log(await response.json())
   }
 
-  return new Response(twiml.toString(), {
+  return new Response("Success", {
     status: 200,
-    headers: {
-      "Content-Type": "text/xml",
-    },
   });
 }
